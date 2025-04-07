4b7b9da9fed1a7f70df6895782ca324f57344a56
Milen Dayana Herrera <milendayana@gmail.com>
2025-04-07 16:14:47 -0500

    Agregando cambios incluyendo submódulo

---
+++ Vista/script.js
@@ -0,0 +1,12 @@
+document.getElementById('boton').addEventListener('click',function(){
+    alert('¡Hola! Has hecho clic en el boton.');
+
+});
+
+document.getElementById('boton 1').addEventListener('click', function() {
+window.location.href='about.html';
+});
+
+document.getElementById('FundacionUKL').addEventListener('click', function() { 
+    window.open('https://www.konradlorenz.edu.co/home-aspirantes/');
+   });
